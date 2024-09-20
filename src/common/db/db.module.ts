ed14664977eb90b01e3542bcca700c30030b20fe
HuynhNgocHoangLoc1 <huynhngochoangloc0211102@gmail.com>
2024-09-20 11:35:32 +0700

    Change env to render

--- src/common/db/db.module.ts
+++ src/common/db/db.module.ts
@@ -4,36 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 // kết nối deploy data ở render
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true, // Sử dụng ConfigModule trên toàn bộ ứng dụng
-//     }),
-//     TypeOrmModule.forRootAsync({
-//       useFactory: (configService: ConfigService) => ({
-//         type: 'postgres',
-//         host: configService.get('DB_HOST'),
-//         port: configService.get<number>('DB_PORT'),
-//         database: configService.get('DB_NAME'),
-//         username: configService.get('DB_USER'),
-//         password: configService.get('DB_PASSWORD'),
-//         url: configService.get('DB_URL'),
-//         entities: [__dirname + './../../entities/*{.ts,.js}'],
-//         synchronize: true,
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false,
-//         },
-//       }),
-//       inject: [ConfigService],
-//     }),
-//   ],
-// })
-
-
-// Kết nối database ở local
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Sử dụng ConfigModule trên toàn bộ ứng dụng
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
@@ -42,15 +17,40 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         database: configService.get('DB_NAME'),
         username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
+        url: configService.get('DB_URL'),
         entities: [__dirname + './../../entities/*{.ts,.js}'],
         synchronize: true,
-        // ssl: {
-        //   require: true,
-        //   rejectUnauthorized: false,
-        // },
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       }),
       inject: [ConfigService],
     }),
   ],
 })
+
+
+// Kết nối database ở local
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRootAsync({
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'postgres',
+//         host: configService.get('DB_HOST'),
+//         port: configService.get<number>('DB_PORT'),
+//         database: configService.get('DB_NAME'),
+//         username: configService.get('DB_USER'),
+//         password: configService.get('DB_PASSWORD'),
+//         entities: [__dirname + './../../entities/*{.ts,.js}'],
+//         synchronize: true,
+//         // ssl: {
+//         //   require: true,
+//         //   rejectUnauthorized: false,
+//         // },
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+// })
 export class DbModule {}
